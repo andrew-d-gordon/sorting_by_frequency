e607dd367f97105b901bb57bdaf9b6b6a4fcaae3
Andrew Gordon <andgordo@ucsc.edu>
2022-02-24 05:52:14 -0800

    Added base case checks to utils

--- server/utils/compareFrequencyList.js
+++ server/utils/compareFrequencyList.js
@@ -1,24 +1,20 @@
-// Compare function to evaluate if entryOne is lesser than entryTwo
-const isLesser = (entryOne, entryTwo) => { // Entries in the form: [word, frequency]
-    //console.log(`In isLesser with: ${entryOne}, ${entryTwo}`)
-    // If frequency of entry one is less than entryTwo, return true
-    if (entryOne[1] < entryTwo[1]) {
-        //console.log(`Returned from isLesser with TRUE`)
-        return true
-    }
+// Compare function to evaluate if entryOne is lesser than entryTwo (entries => [word, frequency])
+const isLesser = (entryOne, entryTwo) => {
+    if (entryOne === null || entryTwo === null) return false;
 
-    // If frequency of entries are equal, return true if entryOne's word is smaller alphabetically
-    if (entryOne[1] === entryTwo[1] && entryOne[0] > entryTwo[0]) {
-        //console.log(`Returned from isLesser with TRUE`)
-        return true;
-    }
-    //console.log(`Returned from isLesser with FALSE`)
+    // If frequency of entry one is less than entryTwo, return true
+    if (entryOne[1] < entryTwo[1]) return true;
+
+    // If frequency of entries are equal, return true if entryOne's word is larger alphabetically
+    if (entryOne[1] === entryTwo[1] && entryOne[0] > entryTwo[0]) return true;
+
     // Entry one is greater than entry two, return false
     return false;
 }
 
 // Compares two entries in wordsList for equality
 const equalEntries = (entryOne, entryTwo) => {
+    if (entryOne === null || entryTwo === null) return false;
     return entryOne[0] === entryTwo[0] && entryOne[1] === entryTwo[1];
 }
 
--- server/utils/searchFrequencyList.js
+++ server/utils/searchFrequencyList.js
@@ -4,6 +4,8 @@ const { isLesser, equalEntries } = require('./compareFrequencyList');
 
 // Implementation of binary search with custom comparison operator (isLesser)
 const binarySearch = (list, item) => {
+    if (item === null || list === null || list.length === 0) return null;
+
     let low = 0;
     let high = list.length - 1;
 
@@ -29,10 +31,12 @@ const binarySearch = (list, item) => {
 // Find appropriate place for item in sorted list (word list), return left neighbor idx
 // List passed in this case will have entirely unique values and a gauranteed spot for the value
 const bisectList = (list, item) => {
+    if (item === null || list === null || list.length === 0) return null;
+
     let low = 0;
     let high = list.length - 1;
 
-    // Base cases, item is smaller/greater than smallest/greatest elems in list
+    // Item is smaller/greater than smallest/greatest elems in list
     if (isLesser(item, list[low]) || isLesser(list[high], item)) {
         return isLesser(item, list[low]) ? 0 : high+1; // Belongs at beginning/end of list
     }
@@ -40,8 +44,7 @@ const bisectList = (list, item) => {
     while (low <= high) { // Implementation requires closure on low and high bounds
         // Narrowed search space to insertion inbetween low and high indices
         if (high-low === 1) {
-            // if (isLesser(list[low], item) && isLesser(item, list[high])) return low + 1 // If list can have non-unique vals
-            return low + 1 // List has entirely unique values and is sorted, only place for item to go
+            if (isLesser(list[low], item) && isLesser(item, list[high])) return low + 1
         }
 
         const mid = Math.floor((low + high) / 2);
@@ -54,7 +57,7 @@ const bisectList = (list, item) => {
         }
     }
 
-    return null; // If not found (note: should never be reached as implemented)
+    return null; // If not found (note: should never be reached with proper input)
 }
 
 module.exports = { binarySearch, bisectList };
